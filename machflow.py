e5cab5659c6cd91d322af7d32f6f47f8089bbae7
Sam Berry <samjamesberry@gmail.com>
2022-05-11 14:38:33 +0100

    Add colourbar

--- machflow.py
+++ machflow.py
@@ -1,13 +1,30 @@
-import json
+import base64
+from io import BytesIO
 
 import meshio
 import pyvista as pv
 from flask import Flask, request, render_template
-from ipywidgets.embed import embed_data, embed_snippet
+from ipywidgets.embed import embed_snippet
 
 from network import load_stl, load_model, predict
 
 app = Flask(__name__)
+
+
+def render_colourbar(limits):
+    pl = pv.Plotter(window_size=[1024, 256], off_screen=True)
+
+    # Need to add an arbitrary mesh to create the color bar
+    plane = [[-5.0, 0.0, 5.0], [-5.0, 0.0, -5.0], [5.0, 0.0, -5.0], [5.0, 0.0, 5.0]]
+    plane_mesh = pv.PolyData(plane).delaunay_2d()
+    pl.add_mesh(plane_mesh, opacity=0.5, clim=limits, scalars=[0, 1, 2, 3])
+    pl.clear()  # Discard mesh
+    pl.add_scalar_bar(title="Cp", height=0.8, width=0.8, position_x=0.1, position_y=0.1, label_font_size=24, title_font_size=32)
+    buf = BytesIO()
+    pl.show(screenshot=buf)
+    img = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+    return img
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -32,5 +49,6 @@ def index():
         pl.add_mesh(mesh)
         pythreejs_renderer = pl.to_pythreejs()
         snippet = embed_snippet([pythreejs_renderer])
+        colourbar = render_colourbar([min(cp), max(cp)])
 
-        return render_template('index.html', result=True, snippet=snippet)
+        return render_template('index.html', result=True, snippet=snippet, colourbar=colourbar)

ea690ff248f0b6401123c25771572f9ce9de3060
Sam Berry <samjamesberry@gmail.com>
2022-05-11 13:09:21 +0100

    Basic 3d render view for results

--- machflow.py
+++ machflow.py
@@ -1,3 +1,5 @@
+import meshio
+import pyvista as pv
 from flask import Flask, request, render_template
 
 from network import load_stl, load_model, predict
@@ -18,5 +20,16 @@ def index():
         model = load_model()
         pred = predict(model, data)
         cp = pred.cpu().detach().float().numpy()[:, 0]
+        mesh = meshio.read(stl_file)
+        mesh.point_data['Cp'] = cp
+        pl = pv.Plotter(off_screen=True)
+        plane = [[-5.0, 0.0, 5.0], [-5.0, 0.0, -5.0], [5.0, 0.0, -5.0], [5.0, 0.0, 5.0]]
+        plane_mesh = pv.PolyData(plane).delaunay_2d()
+        pl.add_mesh(plane_mesh, opacity=0.5)
+        pl.add_mesh(mesh)
+        pl.export_html('pyvista.html')
+        f = open('pyvista.html', "r")
+        lines = f.read()
+        f.close()
 
-        return render_template('index.html', result=cp)
+        return lines
